89e1a2bb440fa32e8286e37cd684a9c8c28234df
Andrew Chen <achen414@gmail.com>
2020-10-29 16:11:36 -0700

    Updated gitignore

--- frontend/reducers/modal_props_reducer.js
+++ frontend/reducers/modal_props_reducer.js
@@ -3,7 +3,11 @@ import { OPEN_MODAL } from '../actions/modal_actions';
 export default function modalPropsReducer(state = null, action) {
     switch (action.type) {
         case OPEN_MODAL:
-            return action.props
+            if (action.props){
+                return action.props;
+            } else {
+                return state;
+            }
         default:
             return state;
     }
