f4d8131d8f32a16caebd052c4153441d1741f0bf
Andrew Chen <achen414@gmail.com>
2021-02-03 03:35:17 -0800

    Added action cable to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://the-cord.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://the-cord.herokuapp.com', 'http://the-cord.herokuapp.com']
 end
